f32a23e65bd8989ed0e8dac5cac004ee19bf155a
Vaibhav Mittal <vaimitta1@publicisgroupe.net>
2020-06-19 23:22:53 +0530

    test cases are updated

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import { getElementByAttr } from '../../shared';
 
 import { Header } from './Header';
